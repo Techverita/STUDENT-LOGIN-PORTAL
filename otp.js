d42826d656662c4a0890de383e8df76122f7e5db
aishik-1999 <86920510+aishik-1999@users.noreply.github.com>
2021-07-22 12:25:45 +0530

    Add files via upload

---
+++ otp.js
@@ -0,0 +1,10 @@
+function clickEvent(first,last){
+			if(first.value.length){
+				document.getElementById(last).focus();
+			}
+		}
+
+        function btnClicked(){
+            document.location.href="otp.html";
+
+        }
